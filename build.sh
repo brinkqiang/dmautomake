bdda2ee53a6dcb00a64326ee599bf0c7f9374e4d
brinkqiang <brink.qiang@gmail.com>
2021-01-25 14:19:39 +0800

    add

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# - install depends tools
+# yum -y install git
+# yum -y install gcc gcc-c++ autoconf libtool automake make
+#
+
+# pushd thirdparty/depends_path
+# libtoolize && aclocal && autoheader && autoconf && automake --add-missing
+# sh configure
+# popd
+
+./configure
+make
+
+# popd
+
+# echo continue && read -n 1
